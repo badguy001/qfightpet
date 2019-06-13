73d72df4dfc18cc5ecec9a14ae6645e8667175dc
dengjiahui <badguy001djh@mail.com>
2019-06-13 23:16:59 +0800

    chrome来获取cookies，用户名密码放在user.txt(格式:username/password)，cookies有效期一般为两天，获取到的cookies放到cookiesx文件中，start.py会循环将cookiesx文件内容复制到cookies.txt文件中，然后按正常流程跑

---
+++ getcookies.py
@@ -0,0 +1,66 @@
+# -*- coding: UTF-8 -*-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+import re
+
+options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--user-data-dir=D:\\user_data')
+options.add_argument('--window-size=1366,768')
+
+
+# options.add_argument('--no-sandbox')
+
+# 查看是否已经登录，没有登录的话在生成login.png进行扫码登录
+def login(username, password):
+    png_name = 'login.png'
+    browser.get(
+        'https://ui.ptlogin2.qq.com/cgi-bin/login?appid=614038002&style=9&s_url=http%3A%2F%2Fdld.qzapp.z.qq.com%2Fqpet%2Fcgi-bin%2Fphonepk%3Fcmd%3Dindex%26channel%3D0')
+    browser.find_element_by_xpath('//input[@id="u" and @name="u"]').send_keys(username)
+    browser.find_element_by_xpath('//input[@id="p" and @name="p"]').send_keys(password)
+    browser.find_element_by_xpath('//div[@id="go"]').click()
+    time.sleep(1)
+    if browser.current_url == 'http://dld.qzapp.z.qq.com/qpet/cgi-bin/phonepk?cmd=index&channel=0':
+        return 1
+    else:
+        return 0
+
+
+def savecookies(filename):
+    uin = browser.get_cookie("uin").get('value')
+    skey = browser.get_cookie("skey").get('value')
+    with open(filename, 'w') as f:
+        f.write('uin=' + uin + '\n' + 'skey=' + skey)
+
+
+def getusers(filename):
+    with open(filename, 'r') as f:
+        us = f.read()
+        p = re.compile(r'([^\n]+)')
+        us = re.findall(p, us)
+        result = []
+        for u in us:
+            u = u.split('/')
+            result.append({"username": u[0], "password": u[1]})
+    return result
+
+
+def browser_close():
+    browser.close()
+    browser.quit()
+
+
+browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options, service_log_path='web.log',
+                           service_args=['--verbose', '--log-path=web.log'])
+
+u_file = 'users.txt'
+us = getusers(u_file)
+
+cookiefile = 'cookies'
+for idx, u in enumerate(us):
+    if login(u.get("username"), u.get("password")):
+        savecookies(cookiefile+str(idx))
+
+browser_close()
