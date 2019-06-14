6adf002ac335bafd505ba75086b02a536bfad651
dengjiahui <badguy001djh@mail.com>
2019-06-14 09:47:44 +0800

    修改爬虫执行方式，原执行方式执行完后线程就退出，导致循环不能重复执行

--- start.py
+++ start.py
@@ -1,13 +1,11 @@
-from scrapy import cmdline
 import os
 import re
 
-
-files=os.listdir('.')
+files = os.listdir('.')
 for file in files:
     p = re.compile(r'^cookies[0-9]+$')
     if os.path.isfile(file) and re.search(p, file) is not None:
-        with open(file,'r') as file_one:
-            with open("cookies.txt",'w') as one:
+        with open(file, 'r') as file_one:
+            with open("cookies.txt", 'w') as one:
                 one.write(file_one.read())
-        cmdline.execute('scrapy crawl qfightpet'.split())
+        os.system('scrapy crawl qfightpet')
