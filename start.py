98cc591018217c76666f2eaa4abc376400a1d1fc
dengjiahui <badguy001djh@mail.com>
2019-06-16 16:13:17 +0800

    飘渺仙境返回

--- start.py
+++ start.py
@@ -5,15 +5,15 @@ import re
 import time
 from scrapy import cmdline
 
-while True:
-    files = os.listdir('.')
-    for file in files:
-        p = re.compile(r'^cookies[0-9]+$')
-        if os.path.isfile(file) and re.search(p, file) is not None:
-            with open(file, 'r') as file_one:
-                with open("cookies.txt", 'w') as one:
-                    one.write(file_one.read())
-            print file
-            # os.system("scrapy crawl qfightpet")
-            cmdline.execute('scrapy crawl qfightpet'.split())   # 调试
-            time.sleep(1)
+# while True:
+files = os.listdir('.')
+for file in files:
+    p = re.compile(r'^cookies[0-9]+$')
+    if os.path.isfile(file) and re.search(p, file) is not None:
+        with open(file, 'r') as file_one:
+            with open("cookies.txt", 'w') as one:
+                one.write(file_one.read())
+        print file
+        os.system("scrapy crawl qfightpet")
+        # cmdline.execute('scrapy crawl qfightpet'.split())   # 调试
+        time.sleep(1)
